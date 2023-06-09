d99fe2c1ab11d3597c7475dd552fbab9fcf3ca1e
ArfazAhamed <arfazahamed31@gmail.com>
2023-06-09 22:53:54 +0600

    password-hide-show-and-confirm-pass

--- src/Pages/SignUp/SignUp.jsx
+++ src/Pages/SignUp/SignUp.jsx
@@ -1,30 +1,75 @@
-import React, { useContext } from 'react';
-import { useForm } from "react-hook-form";
+import React, { useContext, useState } from 'react';
+import * as Yup from 'yup'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
 import { AuthContext } from '../Provider/AuthProvider';
 import Lottie from "lottie-react";
 import signUp from "../../assets/login/118046-lf20-oahmox5rjson.json";
-import { Link } from 'react-router-dom';
-
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { FaEye } from "react-icons/fa";
 
 
 const SignUp = () => {
-    const { createUser} = useContext(AuthContext);
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/login";
+    const [passwordShown, setPasswordShown] = useState(false);
+    const [cPasswordShown, setCPasswordShown] = useState(false);
+    const { createUser, updateUserProfile, logOut } = useContext(AuthContext);
+
+    const validationSchema = Yup.object().shape({
+        name:  Yup.string() 
+         .required('Name is required'),
+         email: Yup.string('Email is required')
+         .required(),
+         photoURL: Yup.string('PhotoURL is required')
+         .required(),
+        password: Yup.string()
+            .required('Password is required')
+            .min(6, 'Password must be at least 6 characters')
+            .matches(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/,'Password must contain at least one lowercase letter, one uppercase letter,  one digit and one special character'
+            ),
+        confirmPassword: Yup.string()
+            .required('Confirm Password is required')
+            .oneOf([Yup.ref('password')], 'Passwords should match!')
+            
+    });
+       const formOptions = { resolver: yupResolver(validationSchema) };
+
     const {
       register,
       handleSubmit,
       reset,
       formState: { errors },
-    } = useForm();
+    } = useForm(formOptions);
   
     const onSubmit = (data) => {
         createUser(data.email, data.password)
         .then((result) => {
           const signUpUser = result.user;
           console.log(signUpUser);
+          updateUserProfile(data.name, data.photoURL)
+        .then(() => {
+          reset();
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "User created successfully",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          logOut()
+          .then(() => {
+            navigate(from, { replace: true });
+          })
+          .catch((error) => console.log(error));
+        })
+        .catch((error) => console.log(error));
         });
       };
 
-
+      
 
     return (
         <>
@@ -44,13 +89,11 @@ const SignUp = () => {
                 <input
                   type="text"
                   name="name"
-                  {...register("name", { required: true })}
+                  {...register("name")}
                   placeholder="name"
-                  className="input input-bordered"
+                  className={`input input-bordered form-control ${errors.name ? 'is-invalid' : ''}`}
                 />
-                {errors.name && (
-                  <span className="text-red-600 mt-1">Name is required</span>
-                )}
+                <div className="invalid-feedback text-red-500 pt-1 ">{errors.name?.message}</div>
               </div>
 
               <div className="form-control">
@@ -59,75 +102,59 @@ const SignUp = () => {
                 </label>
                 <input
                   type="email"
-                  {...register("email", { required: true })}
+                  {...register("email")}
                   name="email"
                   placeholder="email"
-                  className="input input-bordered"
+                  className={`input input-bordered form-control ${errors.email ? 'is-invalid' : ''}`}
                 />
-                {errors.email && (
-                  <span className="text-red-600 mt-1">Email is required</span>
-                )}
+                     <div className="invalid-feedback text-red-500 pt-1 ">{errors.email?.message}</div>
               </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input
-                  type="password"
-                  {...register("password", {
-                    required: true,
-                    minLength: 6,
-                    pattern:
-                      /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-])/,
-                  })}
-                  name="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                />
 
-                {errors.password?.type === "required" && (
-                  <p className="text-red-500">Password name is required</p>
-                )}
-                {errors.password?.type === "minLength" && (
-                  <p className="text-red-500">Password must be 6 characters</p>
-                )}
-                {errors.password?.type === "pattern" && (
-                  <p className="text-red-500">
-                    Password must have one Uppercase, one Lower case,
-                    one Special character
-                  </p>
-                )}
-              </div>
-                 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Confirm Password</span>
-                </label>
-                <input
-                  type="password"
-                  {...register("password", { required: true })}
-                  name="password"
-                  placeholder="Confirm Password"
-                  className="input input-bordered"
-                />
-                 </div>
+             <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                 type={passwordShown ? 'text' : 'password'}
+                {...register("password")}
+                placeholder="Enter your password"
+                className={`input input-bordered form-control ${errors.password ? 'is-invalid' : ''}`}
+                
+              />
+              
+               <FaEye onClick={()=> setPasswordShown(!passwordShown)} className="md:-mt-8 -mt-8  md:ml-72 ml-60"></FaEye>
+               <div className="invalid-feedback text-red-500 pt-5">{errors.password?.message}</div>
 
-              <div className="form-control">
+            </div>
+            <div className="form-control mt-4">
+              <label className="label">
+                <span className="label-text">Confirm password</span>
+              </label>
+              <input
+                type={cPasswordShown ? 'text' : 'password'}
+                {...register("confirmPassword")}
+                placeholder="Enter your password"
+                className={`input input-bordered form-control ${errors.confirmPassword ? 'is-invalid' : ''}`}
+              />
+               
+               <FaEye onClick={()=> setCPasswordShown(!cPasswordShown)} className="md:-mt-8 -mt-8  md:ml-72 ml-60"></FaEye>
+               <div className="invalid-feedback text-red-500 pt-5">{errors.confirmPassword?.message}</div>
+
+
+            </div>
+
+              <div className="form-control mt-4">
                 <label className="label">
                   <span className="label-text">Photo URL</span>
                 </label>
                 <input
                   type="text"
                   name="photoURL"
-                  {...register("photoURL", { required: true })}
+                  {...register("photoURL")}
                   placeholder="photo URL"
-                  className="input input-bordered"
+                  className={`input input-bordered form-control ${errors.photoURL ? 'is-invalid' : ''}`}
                 />
-                {errors.name && (
-                  <span className="text-red-600 mt-1">
-                    Photo URL is required
-                  </span>
-                )}
+                  <div className="invalid-feedback text-red-500 pt-1">{errors.photoURL?.message}</div>
               </div>
               <div className="form-control mt-6">
                 <input
