b533fb7eda8598a392da351f2bfa5bed4b6e1a77
ArfazAhamed <arfazahamed31@gmail.com>
2023-06-10 23:43:42 +0600

    manage user with tanstack and disable button

--- src/Pages/Dashboard/ManageUser/ManageUser.jsx
+++ src/Pages/Dashboard/ManageUser/ManageUser.jsx
@@ -1,9 +1,83 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Swal from 'sweetalert2';
 
 const ManageUser = () => {
+const {data: users = [], refetch} = useQuery(['users'], async()=>{
+    const res = await fetch('http://localhost:5000/users')
+    return res.json()
+})
+
+const handleMakeAdmin = (user) =>{
+fetch(`http://localhost:5000/users/admin/${user._id}`,{
+    method: 'PATCH'
+})
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    if(data.modifiedCount){
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: `${user.name} is an Admin Now!`,
+              showConfirmButton: false,
+              timer: 1500
+            })
+    }
+})
+}
+const handleMakeInstructor = (user) =>{
+fetch(`http://localhost:5000/users/instructor/${user._id}`,{
+    method: 'PATCH'
+})
+.then(res => res.json())
+.then(data => {
+    console.log(data)
+    if(data.modifiedCount){
+            Swal.fire({
+              position: 'top-end',
+              icon: 'success',
+              title: `${user.name} is an Instructor Now!`,
+              showConfirmButton: false,
+              timer: 1500
+            })
+    }
+})
+}
+
     return (
         <div>
-            
+            <div className="overflow-x-auto bg-slate-200">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th>#</th>
+        <th>Name</th>
+        <th>Email</th>
+        <th>Role</th>
+        <th>Action</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        users.map((user, index)=>
+            <tr key={user._id}>
+        <th>{index + 1}</th>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        <td>{user.role === 'admin' ? 'Admin' : user.role === 'instructor' ? 'Instructor' : 'Student'}</td>
+        <td><button disabled={user.role === "admin"} onClick={() =>handleMakeAdmin(user) } className="btn btn-primary btn-sm">Admin</button></td>
+        <td><button disabled={user.role === "instructor"} onClick={() =>handleMakeInstructor(user) } className="btn btn-primary btn-sm">Instructor</button></td>
+      
+      </tr>
+        )
+      }
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,20 @@ import {
 } from "react-router-dom";
 import { router } from './Routes/Router.jsx';
 import AuthProvider from './Pages/Provider/AuthProvider';
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    </QueryClientProvider>
+    
     </AuthProvider>
   </React.StrictMode>,
 )
